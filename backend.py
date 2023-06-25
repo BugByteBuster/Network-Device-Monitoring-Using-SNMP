5f987db2e7413ba49e9b64ca14db8871a8750b96
vidyadhar <vidyadhar.bth@gmail.com>
2023-06-25 00:37:41 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python
 import subprocess
 import sys
 import os
-from subprocess import PIPE
-cmd=[]
-time=[]
-oids =[]
-for i in range(1, len(sys.argv)):
-  cmd.append(sys.argv[i])
 
-cmd=cmd[:0] + ['unbuffer /tmp/A2/prober'] + cmd[0:]
-cmd = cmd[:3] + [-1] + cmd[3:]
+def build_command():
+    cmd = ['unbuffer', '/tmp/A2/prober'] + sys.argv[1:]
+    cmd = cmd[:3] + [-1] + cmd[3:]
+    return " ".join(str(x) for x in cmd)
 
-cmd=" ".join(str(x) for x in cmd)
+def extract_oids():
+    oid_args = sys.argv[3:]
+    return [oid for oid in oid_args]
 
-oid=sys.argv[3:len(sys.argv)]
-for x in oid:
-    oids.append(x)
-#print oids[1]
-a = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, bufsize=1)
+def execute_command(cmd):
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, bufsize=1)
+    return process
 
-for line in iter(a.stdout.readline, b''):
-    s=line.replace("|"," ")
-    s=s.split() #convrtng string to a list
-    rate=s[1:]
-    #print(rate)  
-    for i in range(0,len(oids)):
-        #print oids[i], int(s[0]), float(rate[i])
+def process_output(process, oids):
+    for line in iter(process.stdout.readline, b''):
+        s = line.replace("|", " ")
+        s = s.split()
+        rate = s[1:]
+        for i, oid in enumerate(oids):
+            os.system("curl -i -XPOST 'http://localhost:8086/write?db=A3&u=ats&p=atslabb00&precision=s' --data-binary 'rate,oid=%s value=%f %d'" % (oid, float(rate[i]), int(s[0])))
 
-        os.system ("curl -i -XPOST 'http://localhost:8086/write?db=A3&u=ats&p=atslabb00&precision=s' --data-binary 'rate,oid=%s value=%f %d'"%(oids[i],float(rate[i]),int(s[0])))
+def main():
+    cmd = build_command()
+    oids = extract_oids()
+    process = execute_command(cmd)
+    process_output(process, oids)
 
-         
-
+if __name__ == '__main__':
+    main()
